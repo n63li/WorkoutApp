880856d16fbeb33e323d490a44285ee2556b0be4
Nathan Li <n63li@edu.uwaterloo.ca>
2018-11-24 11:09:27 -0500

    Initial commit

---
+++ app/src/main/java/com/example/nathan/workoutapp/WorkoutSelectionActivity.java
@@ -0,0 +1,13 @@
+package com.example.nathan.workoutapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WorkoutSelectionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_workout_selection);
+    }
+}
